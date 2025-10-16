c92b03547f87392b50ff91f3518e4e64d10b72a4
Jonathan Alca <jhona.alca@gmail.com>
2025-10-16 05:17:40 -0500

    Initial Commit

---
+++ MedalsProfileAppTests/MedalsProfileAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedalsProfileAppTests.swift
+//  MedalsProfileAppTests
+//
+//  Created by Jhona Alca on 16/10/25.
+//
+
+import Testing
+@testable import MedalsProfileApp
+
+struct MedalsProfileAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
